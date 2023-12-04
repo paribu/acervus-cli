60e5e680f492e5c388ac29708758a3e217dc662e
somestry <giray.aytar@xprclub.com>
2023-12-04 16:45:37 +0300

    Implement config system
    - Installed viper package
    - Config system is for contributors to use, users won't need it.

---
+++ src/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+var (
+	AuthServiceURL           string
+	ProjectManagerServiceURL string
+)
+
+func init() {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+
+	// Do not handle errors here because it is not mandatory
+	// to have a config file. It should work fine with default values.
+	viper.ReadInConfig()
+
+	setDefaultValues()
+	configureVariables()
+}
+
+func setDefaultValues() {
+	viper.SetDefault("auth.url", "")           // TODO fill after it is determined
+	viper.SetDefault("projectManager.url", "") // TODO fill after it is determined
+}
+
+func configureVariables() {
+	AuthServiceURL = viper.GetString("auth.url")
+	ProjectManagerServiceURL = viper.GetString("projectManager.url")
+}
