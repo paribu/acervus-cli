127e4294f4dca00a6d0e4176bda63850d39f9b34
Samir Lahluh <samir.lahluh@paribu.com>
2023-12-19 11:41:43 +0300

    chore: export auth-api structure from api

--- src/api/client_auth.go
+++ src/api/client_auth.go
@@ -6,21 +6,21 @@ import (
 	"github.com/paribu/acervus-cli/src/config"
 )
 
-type authAPI struct {
+type AuthAPI struct {
 	apiClient
 	BaseURL string
 }
 
-func NewAuthAPI() *authAPI {
-	return &authAPI{
+func NewAuthAPI() *AuthAPI {
+	return &AuthAPI{
 		BaseURL: config.AuthServiceURL,
 	}
 }
 
-func (a *authAPI) makeAPIRequest(method, path string, reqData RequestData) ([]byte, error) {
+func (a *AuthAPI) makeAPIRequest(method, path string, reqData RequestData) ([]byte, error) {
 	return a.makeRequest(method, fmt.Sprintf("%s/%s", a.BaseURL, path), reqData)
 }
 
-func (a *authAPI) makeAuthenticatedAPIRequest(method, path string, reqData RequestData) ([]byte, error) {
+func (a *AuthAPI) makeAuthenticatedAPIRequest(method, path string, reqData RequestData) ([]byte, error) {
 	return a.makeAuthenticatedRequest(method, fmt.Sprintf("%s/%s", a.BaseURL, path), reqData)
 }
